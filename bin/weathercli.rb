f6c51d7f2ed7914d613eeca09a668b777d26bf74
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-07-06 16:31:11 -0400

    read me included

--- bin/weathercli.rb
+++ bin/weathercli.rb
@@ -1,34 +1,29 @@
+#!/usr/local/bin/ruby
 require_relative '../lib/weather.rb'
 
 arg = "x"
 arg =  ARGV[0].gsub("-","") if ARGV[0]
 
+weather = Weather.new
 
 case arg
 when 'x'
-weather = Weather.new
 	weather.twoDayForecast
 when 't'
-weather = Weather.new
 	weather.tomorrow
 when 'td'
-weather = Weather.new
 	weather.tomorrowInfo
 when 'n'
-weather = Weather.new
 	weather.today
 when 'nd'
-weather = Weather.new
 	weather.todayInfo
 when 'w'
-weather = Weather.new
 	weather.sevenDayForecast
 when 'wd'
-weather = Weather.new
 	weather.sevenDayInfo
 when 'h'
+	weather.help
 when 'help'
-weather = Weather.new
 	weather.help
 else
 	puts "Invalid input, please use -h or --help for help"
